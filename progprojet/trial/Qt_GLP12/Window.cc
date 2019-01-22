e05fbba21f48dce5e0fb51fd4da9de8593209d85
Unknown <youssef.saied@epfl.ch>
2019-01-22 03:03:16 +0000

    minor and plan.pdf

--- progprojet/trial/Qt_GLP12/Window.cc
+++ progprojet/trial/Qt_GLP12/Window.cc
@@ -28,6 +28,9 @@ Window::Window()
     xSlider        = createSlider();
     ySlider        = createSlider();
     zSlider        = createSlider();
+    xselector      = createdoubleSpin(0.1);
+    yselector      = createdoubleSpin(0.1);
+    zselector      = createdoubleSpin(0.1);
     magnetselector = createSpin();
     labels         = new QLabel(this);
     labels->setText("Change Magnet");
@@ -38,6 +41,10 @@ Window::Window()
     connect(xSlider, SIGNAL(valueChanged(int)), glWidget, SLOT(setXH(int)));
     connect(ySlider, SIGNAL(valueChanged(int)), glWidget, SLOT(setYH(int)));
     connect(zSlider, SIGNAL(valueChanged(int)), glWidget, SLOT(setZH(int)));
+    connect(xselector, SIGNAL(valueChanged(double)), glWidget, SLOT(setXH(double)));
+    connect(yselector, SIGNAL(valueChanged(double)), glWidget, SLOT(setYH(double)));
+    connect(zselector, SIGNAL(valueChanged(double)), glWidget, SLOT(setZH(double)));
+    // 6 connections to H value changed
     connect(magnetselector, SIGNAL(valueChanged(int)), glWidget, SLOT(selectmagnet(int)));
     connect(glWidget, SIGNAL(magnetselected(int)), this, SLOT(setmagnetT(int)));
     connect(flipangle, SIGNAL(clicked()), glWidget, SLOT(flipmagnet()));
@@ -47,10 +54,13 @@ Window::Window()
     mainLayout->addWidget(glWidget, 0, 0);
     mainLayout->addWidget(labelO, 9, 0);
     mainLayout->addWidget(xSlider, 0, 1);
+    mainLayout->addWidget(xselector, 8, 1);
     mainLayout->addWidget(labelx, 9, 1);
     mainLayout->addWidget(ySlider, 0, 2);
+    mainLayout->addWidget(yselector, 8, 2);
     mainLayout->addWidget(labely, 9, 2);
     mainLayout->addWidget(zSlider, 0, 3);
+    mainLayout->addWidget(zselector, 8, 3);
     mainLayout->addWidget(labelz, 9, 3);
     mainLayout->addWidget(labels, 0, 5);
     mainLayout->addWidget(magnetselector, 0, 6);
@@ -87,10 +97,10 @@ QSpinBox * Window::createSpin()
     return spin;
 }
 
-QDoubleSpinBox * Window::createdoubleSpin()
+QDoubleSpinBox * Window::createdoubleSpin(double step)
 {
     QDoubleSpinBox * spin = new QDoubleSpinBox();
-    spin->setRange(0, 100);
-    spin->setSingleStep(0.01);
+    spin->setRange(0, 10000);
+    spin->setSingleStep(step);
     return spin;
 }
